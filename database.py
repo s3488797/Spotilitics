1076b8b28bdc7d788a440b0c851cc485fded28c8
Declan McDonald <s3488797@student.rmit.edu.au>
2019-01-23 17:26:09 +1100

    Added database structure for user object

--- database.py
+++ database.py
@@ -6,4 +6,19 @@ db = SQLAlchemy()
 
 def init_databse():
     app = Flask(__name__)
-    
+    app.config.from_pyfile('config.py')
+    app.config.setdefault('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    db.init_app(app)
+    with app.app_context():
+
+#Models
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    spotify_id = db.Column(db.String(255))
+    display_name = db.Column(db.String(255))
+    joined = db.Column(db.DateTime())
+    last_check = db.Column(db.DateTime())
+    listens = db.Column(db.Integer)
+    auth_token = db.Column(db.String(255))
+    refresh_token = db.Column(db.)
